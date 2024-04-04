7ed9c38702e48b7e70b444feec9b7e4f5bc2f9d5
Thimea <84723445+Thimea5@users.noreply.github.com>
2024-04-04 16:26:36 +0200

    Ajout Designation

---
+++ Projet-Blue-main/Blue/Accueil.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blue
+//
+//  Created by Guillaume Panighetti on 02/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+    
