0826f2af0ff8e7d86a0b607a7da4493830bca197
Guillaume-Panighetti <150042209+Guillaume-Panighetti@users.noreply.github.com>
2024-04-11 13:51:02 +0200

    presque fini
    (regarde ListeSoirees pr savoir ce qu'il reste à faire)

---
+++ Projet-Blue/Blue/ListeSoirees.swift
@@ -0,0 +1,37 @@
+//
+//  ListeSoirees.swift
+//  Blue
+//
+//  Created by Guillaume Panighetti on 08/04/2024.
+//
+
+import UIKit
+
+class ListeSoirees: UIViewController {
+
+    @IBOutlet weak var view_listeSoiree: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+
+}
+
+
+/* à faire :
+ 
+ retirer l'élément de la liste "personnesAvecVéhicules" dès qu'il a été designé (pour l'instant il se retire du tableau mais dès qu'on reclique sur bt_lancerRoue le tableau se remet au point de départ)
+ 
+ afficher des nouveaux boutons dans la liste quand on clique sur "commencer" dans la vue "adresse soirées" (à faire)
+ 
+ 
+ voir si on peut conserver les données quand on ferme l'appli (optionnel) (en vrai pas besoin)
+ */
